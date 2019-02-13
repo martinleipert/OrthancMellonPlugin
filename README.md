37c36c2f966c2a37606904e0c30907e410ac9ff7
martin <martin.leipertqt-online.de>
2019-02-13 01:05:20 +0100

    Added Comments and refactored Code. Added an introduction about the project to README.md.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# OrthancMellonPlugin
+# Orthanc Mellon 
+
+Extends the Orthanc explorer by some functionality necessary to use it with Single Sign On. 
+Additionally some functionality for the communication with a WebService for the advanced Authorization Plugin is added.
+
+For the adding of functionality embedded ressources are used and provided by the plugin. 
+To compile the plugin it is necessary to put the directory into the Plugins folder of the Orthanc project.
+To compile it without this project directory the CMakeListsFile has to be modified and the correct path to the orthanc sources has to be provided.
+
+## Functionality provided
+
+In the file Resources/OrthancExplorer.js:
+
+- **Logout Button** with a link to the mod_mellon logout link
+- At each patient page the possibility to grant access to other users or remove the access
+
+In the file Resources/PrivilegeAdministration.html: 
+- A static html page which allows to modify the group users belong to
--- Resources/OrthancExplorer.js
+++ Resources/OrthancExplorer.js
@@ -1,20 +1,30 @@
 
+// By Martin Leipert
+// martin.leipert@fau.de
+
+
+// The logout button on top of the page
 $(document).live('pagebeforecreate', function() {
+    // So that the logout is only created once
+    // -> If we don't check this an additional logout button will be created with every click on the explorer
   if ($('#logout-button').length) {
       return;
   }
     var b1 = $('<a>')
       .attr('id', 'logout-button')
       .attr('data-role', 'button')
+      // Most important -> Point to the mellon logout
       .attr('href', '/mellon/logout?ReturnTo=http://orthanc.cic.userena.cl/Orthanc')
       .attr('data-theme', 'e')
       .text('Logout');
 
+    // Add to the right button section
     $('div.ui-btn-right').append(b1);
 });
 
 
-
+// Add to the patient section of the orthanc browser
+// a set of buttons which allows to add or remove access to this patient to other users
 $('#patient').live('pagebeforecreate', function() {
   //$('#series-preview').parent().remove();
 
@@ -24,10 +34,11 @@ $('#patient').live('pagebeforecreate', function() {
       // var resp = JSON.parse(responseText);
       // var users = resp.users;
 
+      // Create a list
       var u1 = $('<ul>')
         .attr('class', 'ui-listview ui-listview-inset ui-corner-all ui-shadow');
 
-
+      // First list element -> A title text
       var l1 = $('<li>')
         .attr('role', 'heading')
         .attr('data-role', 'list-divider')
@@ -35,7 +46,7 @@ $('#patient').live('pagebeforecreate', function() {
         .text('Access Management');
       u1.append(l1);
 
-
+      // Second dropdown a button which gets filled lazily
       var l2 = $('<li>')
         .attr('class', 'ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li');
       var s1 = $('<select>')
@@ -44,9 +55,10 @@ $('#patient').live('pagebeforecreate', function() {
       l2.append(s1);
       u1.append(l2);
 
-
+      // List element 4
       var l4 = $('<li>')
         .attr('class', 'ui-btn-text');
+      // Grant access Button
       var b1 = $('<a>')
         .attr('data-role', 'button')
         .attr('href', '#')
@@ -57,12 +69,15 @@ $('#patient').live('pagebeforecreate', function() {
       l4.append(b1)
         .attr('class', 'ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li');
       u1.append(l4);
+
+      // Add a onclick function to the button
       b1.click(grant_access_click);
 
 
-
+      // List element 5 another button
       var l5 = $('<li>')
         .attr('class', 'ui-btn-text');
+      // Button to undo access right
       var b2 = $('<a>')
         .attr('data-role', 'button')
         .attr('href', '#')
@@ -74,6 +89,7 @@ $('#patient').live('pagebeforecreate', function() {
 
       u1.append(l5);
 
+      // Also add a function here
       b2.click(retreat_access_click);
 
       u1.insertBefore($('#patient-delete').parent().parent());
@@ -81,7 +97,8 @@ $('#patient').live('pagebeforecreate', function() {
 
       var data;
 
-      // Call the function to fill the select
+      // 1. Call the web service which transmits the selectable user ids
+      // 2. Call the function to fill the select
       var responseText = jQuery.getJSON(
         'http://localhost:8000/userAdministration/',
         data,
@@ -91,6 +108,7 @@ $('#patient').live('pagebeforecreate', function() {
 
 });
 
+// Extract the patients's orthanc id from the url
 function getPatientId() {
 
       var url_string = window.location.href;
@@ -102,7 +120,8 @@ function getPatientId() {
 
 
 
-
+// Fill the select during loading
+// A callback method
 function fill_select(data) {
   var s1 = $("#user-select");
 
@@ -112,14 +131,17 @@ function fill_select(data) {
 
 }
 
+
+// To grant an access privilege sent a json request to the web service
 function grant_access_click() {
   var json_dict = {};
 
+  // Get the user select to extract the user's id
   select = document.getElementById("user-select");
-
-
-  json_dict["orthanc-id"] = getPatientId();
   json_dict["MOD_MELLON_uid"] = select.value;
+
+  // Get the patient id from the url
+  json_dict["orthanc-id"] = getPatientId();
 
   // construct an HTTP request
   var xhr = new XMLHttpRequest();
@@ -131,13 +153,17 @@ function grant_access_click() {
   xhr.send(json_str);
 }
 
+
+// To retreat the access privilege sent a json request to the web service
 function retreat_access_click() {
   var json_dict = {};
 
+  // Get the user select to extract the user's id
   select = document.getElementById("user-select");
-
-  json_dict["orthanc-id"] = getPatientId();
   json_dict["MOD_MELLON_uid"] = select.value;
+
+  // Get the patient id from the url
+  json_dict["orthanc-id"] = getPatientId();
 
   // construct an HTTP request
   var xhr = new XMLHttpRequest();
